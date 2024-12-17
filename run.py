7234c54810ee950d496ee04f1f1d9d36a5d85145
Juan Ponce <poncejuan.-@hotmail.com>
2024-12-17 00:55:16 -0300

    Configuracion para ejecutar en produccion

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from src import app
 
 #Descomentar para levantar localmente
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
--- src/__init__.py
+++ src/__init__.py
@@ -2,8 +2,8 @@ from flask import Flask
 from flask_pymongo import PyMongo
 
 app = Flask(__name__)
-app.config['MONGO_URI']='mongodb://localhost:27017/gamesdb'    #Direccion de prueba local
-# app.config['MONGO_URI']='mongodb://mongo-gamesdb:27017/gamesdb'  #Direccion de conexion entre contenedores con contenedor con MongoDB
+# app.config['MONGO_URI']='mongodb://localhost:27017/gamesdb'    # Descomentar para utilizar localmente con contenedor de MongoDB 
+app.config['MONGO_URI']='mongodb://mongo-gamesdb:27017/gamesdb'  #Direccion de conexion entre contenedores con contenedor con MongoDB
 mongo = PyMongo(app)
 
 from src import routes
